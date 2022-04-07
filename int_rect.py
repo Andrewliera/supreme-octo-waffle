7b69516177abb2714353a955aa8134e259fd5f92
Andrew Gonzaga <andrewliera15@gmail.com>
2022-04-07 12:08:54 -0400

    new schedule, new commit

---
+++ int_rect.py
@@ -0,0 +1,18 @@
+#write a program which tests if two rectangles have a non empty intersection
+# book hint: think of the X and Y dimensions independently 
+# according to the book, the problem is unspecified enough 
+# so that we can also take into account the border of the rectangle
+# in otherwords, two rectangles with touching corners is enough to count as "intersecting"
+# good book approach:
+# focus on conditions where the rectangles are guaranteed NOT intersecting 
+# another way to think about it 
+# 
+# 
+# 
+# 
+# 
+# #
+
+
+def intersect_rectangle(R1, R2):
+    return null
