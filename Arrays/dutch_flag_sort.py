7d6dc47cf58157cf80881d43de28b2ebc6eefcb8
Andrew Gonzaga <andrewliera15@gmail.com>
2022-04-10 12:01:29 -0400

    added 3rd function to flag problem

--- Arrays/dutch_flag_sort.py
+++ Arrays/dutch_flag_sort.py
@@ -6,7 +6,7 @@
 # book hint: Think about partition step in quicksort 
 # > in other words the book is asking us to solve the problem with
 # a quick sort algorithm 
-# if element A[j] is less than our pivot swap them
+# 
 # 
 # 
 # #
@@ -46,5 +46,29 @@ def dutch_flag_sort_two(pivot_index, A):
         elif A[i] > pivot:
             A[i], A[larger] = A[larger], A[i]
             larger -= 1
-# this time the algorithm seperated elements in 1 pass rather than a nested one
+# this time the algorithm seperated elements in 2 passes rather than two nested passes
 # for O(n)
+
+def dutch_flag_sort_three(pivot_index, A):
+    pivot = A[pivot_index]
+    smaller,equal, larger = 0, 0, len(A)
+
+    while equal < larger:
+        if A[equal] < pivot:
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller, equal = smaller + 1, equal + 1
+        elif A[equal] == pivot:
+            equal += 1
+        else: # <-else A[equal] > pivot
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
+# and finally the book classifies the elements in a single pass
+# we will have 4 subarrays:
+# elements less than pivot
+# elements equal to pivot
+# unlcassified elements
+# elements greater than pivot
+# this time we sort from classified into the other three subarrays
+# for O(n)
+
+
